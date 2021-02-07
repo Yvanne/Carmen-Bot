baeb9342cf9433fd741eaf60aa3c7fbbb1bc6bff
yseuhou <yseuhou@umd.edu>
2021-02-07 04:04:09 -0500

    added carmenita

---
+++ carmen/commands/oui.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'oui',
+    description: 'This is a oui command',
+    execute(message, args) {
+        message.channel.send('https://www.youtube.com/watch?v=Cap0BOzizbw');
+
+    }
+}
