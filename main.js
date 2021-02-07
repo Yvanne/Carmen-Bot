a4e60dc1136d2ed0609ceecb5f6013b6fe3c3802
yseuhou <yseuhou@umd.edu>
2021-02-07 06:03:27 -0500

    comit

---
+++ main.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js')
+
+
+
+const client = new Discord.Client();
+
+const prefix = '$';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', ()=>{
+    console.log('testing is alive')
+})
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    // allows to have multiple commands
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase();
+
+    if (command === 'oui') {
+        client.commands.get('oui').execute(message, args)
+    }
+    else if (command == 'alain') {
+        message.channel.send('https://www.youtube.com/watch?v=YihmpQdTj54')
+    }
+
+
+})
+
+
+
+// GIVING ACCESS TO THE BOT
+client.login('ODA3ODM5MTI0NDA0MzA2MDAx.YB900A.t0BZ_oRjR2cDMJsfOg3bBf_44x4')
